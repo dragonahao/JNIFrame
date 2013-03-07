73d22e1f16d85e20cccaf1ee830dcda04e1edc17
haizhu12345@gmail.com <haizhu12345@gmail.com>
2013-03-07 17:01:17 +0800

    dsfasf

---
+++ jni/MyLog.h
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string.h>
+#include <jni.h>
+#include <android/log.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/* Header for class com_brunjoy_Setting */
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "setting", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "setting", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "setting", __VA_ARGS__))
+
+
--- obj/local/armeabi/objs/jni_framera/com_brunjoy_jni_JNI.o.d
+++ obj/local/armeabi/objs/jni_framera/com_brunjoy_jni_JNI.o.d
@@ -15,7 +15,36 @@
  /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
  /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
  /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
- /cygdrive/d/EclipsePro/JniFrame/jni/FileOperate.h
+ /cygdrive/d/EclipsePro/JniFrame/jni/FileOperate.h \
+ /cygdrive/d/EclipsePro/JniFrame/jni/MyLog.h \
+ /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/string.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/string.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stddef.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/stdlib.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/strings.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/memory.h \
+ /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stdio.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/stdio.h \
+ /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/assert.h \
+ /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/assert.h
 
 /cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/jni.h:
 
@@ -48,3 +77,61 @@
 /cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
 
 /cygdrive/d/EclipsePro/JniFrame/jni/FileOperate.h:
+
+/cygdrive/d/EclipsePro/JniFrame/jni/MyLog.h:
+
+/cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/string.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/string.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+/cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/stdlib.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/stdio.h:
+
+/cygdrive/d/android-ndk-r8/sources/cxx-stl/stlport/stlport/assert.h:
+
+/cygdrive/d/android-ndk-r8/platforms/android-14/arch-arm/usr/include/../include/assert.h:
